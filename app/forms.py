f3163976a5314109f6f1d951222f9a664556c16c
Agatha Uy <yuahtaga@gmail.com>
2016-12-24 14:08:08 +0800

    Up to Part 6: Profile Page & Avatars

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,11 @@
-from flask_wtf import Form
-from wtforms import StringField, BooleanField
-from wtforms.validators import DataRequired
+from flask_wtf import FlaskForm
+from wtforms import StringField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     openid = StringField('openid', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
+
+class EditForm(FlaskForm):
+    nickname = StringField('nickname', validators=[DataRequired()])
+    about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
--- app/views.py
+++ app/views.py
@@ -1,13 +1,28 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask_login import login_user, logout_user, current_user, login_required
-from app import app, db, lm, old
-from .forms import LoginForm
+from app import app, db, lm, oid
+from .forms import LoginForm, EditForm
 from .models import User
+from datetime import datetime
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+# checked before any other functions in views.py are loaded
+@app.before_request
+def before_request():
+    g.user = current_user #current user global set by Flask-login
+    if g.user.is_authenticated:
+        g.user.last_seen = datetime.utcnow()
+        db.session.add(g.user)
+        db.session.commit()
 
 @app.route('/')
 @app.route('/index')
+@login_required # will ensure that this page is only seen by logged in users
 def index():
-    user = {'nickname': 'Miguel'} # fake user
+    user = g.user
     posts = [ # fake array of posts
         {
             'author': {'nickname': 'John'},
@@ -25,7 +40,9 @@ def index():
                             posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
+@oid.loginhandler
 def login():
+    print ("g.user: {}".format(g.user))
     if g.user is not None and g.user.is_authenticated:
         return redirect(url_for('index'))
 
@@ -39,9 +56,6 @@ def login():
                             form=form,
                             providers=app.config['OPENID_PROVIDERS'])
 
-@lm.user_loader
-def load_user(id):
-    return User.query.get(int(id))
 
 @oid.after_login
 def after_login(resp):
@@ -53,7 +67,7 @@ def after_login(resp):
         nickname = resp.nickname
         if nickname is None or nickname == "":
             nickname = resp.email.split('@')[0]
-        user = User(nickname=name, email=resp.email)
+        user = User(nickname=nickname, email=resp.email)
         db.session.add(user)
         db.session.commit()
     remember_me = False
@@ -61,4 +75,42 @@ def after_login(resp):
         remember_me = session['remember_me']
         session.pop('remember_me', None)
     login_user(user, remember = remember_me)
-    return redirect(request.args.get('next' or url_for('index')))
+    return redirect(request.args.get('next') or url_for('index'))
+
+@app.route('/logout')
+def logout():
+    logout_user() #custom function of oid?
+    return redirect(url_for('index'))
+
+# nickname is an argument
+@app.route('/user/<nickname>')
+@login_required
+def user(nickname):
+    user = User.query.filter_by(nickname=nickname).first()
+    if user == None:
+        flash('User %s not found.' % nickname)
+        return redirect(url_for('index'))
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html',
+                            user=user,
+                            posts=posts)
+
+@app.route('/edit', methods=['GET', 'POST'])
+@login_required
+def edit():
+    form = EditForm()
+    if form.validate_on_submit():
+        g.user.nickname = form.nickname.data
+        g.user.about_me = form.about_me.data
+        db.session.add(g.user)
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit'))
+    else:
+        form.nickname.data = g.user.nickname
+        form.about_me.data = g.user.about_me
+    return render_template('edit.html', form=form)
+
